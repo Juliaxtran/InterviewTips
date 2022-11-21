954dd020146dc7564ff05ba4d613ce98e2e04932
Julia Tran <julia.kim.tran@hotmail.com>
2022-11-21 13:30:24 -0500

    Undefined vs null

---
+++ 05_undefinedvsnull.js
@@ -0,0 +1,10 @@
+// Whats the difference between undefied and null?
+
+// undefined is a variable that has not been assigned a value
+// null is an assignment value. It can be assigned to a variable as a representation of no value.
+
+
+// What are  some Javascript data types?
+
+// Primitive data types: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+// Non-primitive data types: Object, Array, Function
