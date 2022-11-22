bca4685a6fb9eac98f59d23d71157b20d96c6fba
Julia Tran <julia.kim.tran@hotmail.com>
2022-11-22 12:34:44 -0500

    Problems with numbers in javascript

---
+++ 12_numberissues.js
@@ -0,0 +1,7 @@
+// What are some issues with numbers in javascript you
+
+console.log(0.2 + 0.1) // 0.30000000000000004
+
+// Use Math.round() to round a number to the nearest integer
+// use toFixed() to round a number to a certain number of decimal places
+// deal with numbers like money
