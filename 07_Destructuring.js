7f6e957607c6ea1b162231bb7d23b8cce894602a
Julia Tran <julia.kim.tran@hotmail.com>
2022-11-22 11:53:13 -0500

    Destructuring Question

--- 07_Destructuring.js
+++ 07_Destructuring.js
@@ -6,3 +6,16 @@
 const dob = [10, 25, 1987];
 
 // want the code to be self documenting
+// good for readability
+const [day, month, year] = dob;
+console.log('day', day);
+
+const user = {
+  f: 'Dylan',
+  l: 'Isreal',
+}
+
+// Can we redefine
+// provide intent
+// code readability
+const {f: firstName, l: lastName} = user;
