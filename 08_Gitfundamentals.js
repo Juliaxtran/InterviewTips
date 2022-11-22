9f80cffe4ad461f9fe43e106eb4887a8de6d25f9
Julia Tran <julia.kim.tran@hotmail.com>
2022-11-22 12:10:37 -0500

    Css selectors

---
+++ 08_Gitfundamentals.js
@@ -0,0 +1,16 @@
+// What is Git, and why do you use it? What are the common command you use?
+// Git is a version control system. It allows you to track changes to your code. It allows you to revert back to a previous version of your code. It allows you to collaborate with other developers on the same code base. It allows you to create branches of your code. It allows you to merge branches of your code. It allows you to create pull requests. It allows you to create issues. It allows you to create a wiki. It allows you to create a project board. It allows you to create a repository. It allows you to create a commit. It allows you to create a push. It allows you to create a pull. It allows you to create a fetch. It allows you to create a merge. It allows you to create a rebase. It allows you to create a stash. It allows you to create a tag. It allows you to create a branch. It allows you to create a checkout. It allows you to create a clone. It allows you to create a remote. It allows you to create a origin. It allows you to create a upstream. It allows you to create a fork. It allows you to create a pull request. It allows you to create a issue. It allows you to create a wiki. It allows you to create a project board. It allows you to create a repository. It allows you to create a commit. It allows you to create a push. It allows you to create a pull. It allows you to create a fetch. It allows you to create a merge. It allows you to create a rebase. It allows you to create a stash. It allows you to create a tag. It allows you to create a branch. It allows you to create a checkout. It allows you to create a clone. It allows you to create a remote. It allows you to create a origin. It allows you to create a upstream. It allows you to create a fork. It allows you to create a pull request. It allows you to create a issue. It allows you to create a wiki. It allows you to create a project board. It allows you to create a repository. It allows you to create a commit. It allows you to create a push. It allows you to create a pull. It allows you to create a fetch. It allows you to create a merge. It allows you to create a rebase. It allows you to create a stash. It allows you to create a tag. It allows
+
+
+// most popular form of version control system.
+// keep track of various code changes.
+// revert back to previous version of code.
+// collaborate with other developers on the same code base.
+
+// git init
+// create a branch. git checkout -b branchName
+// git status
+// git add .
+// git commit -m 'message'
+// git pull
+// git push
---
+++ 09_Agile.js
@@ -0,0 +1,7 @@
+// Wha tis Agile? What is scrum ?
+
+// Scrum
+// Agile is a methodology for software development. It is a framework for developing, delivering, and sustaining complex products.
+// Scrum is a framework within the Agile methodology for developing, delivering, and sustaining complex products.
+// Follows the tenents of velocity. Have an actionable amount to work in our sprints. Stands up in the morning with the goal of figuring out what is blocking us, and what it is that we are doing, what we did. Keep the team accountable.
+// Agile and Scrum is always about team agreement.
